fd5a847e482cad686567ea7e37c4f65d2adcf458
Rob <21dekkir@gmail.com>
2017-10-27 22:06:05 +0300

    Driver, добавлено: init, finish Добавлен тест тайтла

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,4 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static java.lang.Thread.sleep;
+
+
 public class Driver {
-    int a;
+    static WebDriver driver = null;
+
+
+    static void init(){
+       // System.setProperty("geckodriver", "C:\\Program Files\\Java\\jdk1.8.0_111\\bin");
+        driver = new FirefoxDriver();
+        Driver.driver.get("https://223.rts-tender.ru/supplier/auction/Trade/Search.aspx");
+        wait(1);
+    }
+
+    static void finish(){
+        driver.close();
+    }
+
+    private static void wait(int sec){
+        try{
+            sleep(sec * 1000);
+        } catch (InterruptedException ie){
+            System.out.println(ie);
+        }
+    }
 
 }
--- src/test/java/TestSelenium.java
+++ src/test/java/TestSelenium.java
@@ -1,40 +1,26 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
 
-import static java.lang.Thread.sleep;
 
 public class TestSelenium {
-    WebDriver wd = null;
 
 
-    @BeforeMethod()
+    @BeforeMethod
     public void setUp(){
-        //System.setProperty("geckodriver", "C:\\Program Files\\Java\\jdk1.8.0_111\\bin");
-        wd = new FirefoxDriver();
+        Driver.init();
     }
-
-    @AfterMethod()
-    public void closed(){
-        wd.close();
+    @AfterMethod
+    public void close(){
+        Driver.finish();
     }
 
     @Test
-    public void testMainPage() throws InterruptedException {
-        wd.get("http://irissh.beon.ru/");
-        sleep(5000);
-        String s = wd.getTitle();
-        if(s.equals("Future")) System.out.println("true");
-        else System.out.println("false");
-        List<WebElement> we = wd.findElements(By.cssSelector("*.blog"));
-        for (WebElement aWe : we) {
-            System.out.println(aWe.toString());
-        }
+    public void testTitle()  {
+        Assert.assertEquals("Поиск закупок", Driver.driver.getTitle());
     }
 }
